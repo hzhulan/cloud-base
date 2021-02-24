184bbcf2336b23d344801ab736cc4f0142644fd8
hzhu <zgjszhuhui@163.com>
2021-02-24 16:00:08 +0800

    修改网关

--- gateway/src/main/java/com/fh/config/GateWayConfig.java
+++ gateway/src/main/java/com/fh/config/GateWayConfig.java
@@ -5,10 +5,13 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * 代码配置路由，屏蔽
+ */
+//@Configuration
 public class GateWayConfig {
 
-    @Bean
+//    @Bean
     public RouteLocator customerRouterLocator(RouteLocatorBuilder routeLocatorBuilder) {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
         routes.route("path_route_atguigu",
