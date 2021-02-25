198491e76a14a6510de79149c57ee51f1e51cfe1
hzhu <zgjszhuhui@163.com>
2021-02-25 18:57:04 +0800

    message

---
+++ stream-provider8001/src/main/java/com/fh/md/controller/SendMessageController.java
@@ -0,0 +1,19 @@
+package com.fh.md.controller;
+
+import com.fh.md.service.IMessageProvider;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @RequestMapping("/sendMsg")
+    public String sendMessage() {
+        return messageProvider.send();
+    }
+}
